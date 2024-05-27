44be866eada37eda48cd42fefba622c0416b9abe
Shadrack <skmutai6@gmail.com>
2024-05-27 22:30:47 +0300

    lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list): # Initialize an empty list to store the matching words
+        matching = []
+
+        matching = [word for word in word_list if sorted(self.word) == sorted(word)]
+
+        return matching
+    
